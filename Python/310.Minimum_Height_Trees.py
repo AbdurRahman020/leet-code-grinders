0373c5ad41944248ac17c8874e2ba50e1ba997d9
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-04-23 18:04:06 +0500

    update

---
+++ Python/310.Minimum_Height_Trees.py
@@ -0,0 +1,31 @@
+from collections import defaultdict, deque
+
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: list[list[int]]) -> list[int]:
+        if n == 1:
+            return [0]
+
+        graph = defaultdict(list)
+
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+
+        leaves = deque(node for node in graph if len(graph[node]) == 1)
+
+        while n > 2:
+            n -= len(leaves)
+            for _ in range(len(leaves)):
+                leaf = leaves.popleft()
+                neighbor = graph[leaf].pop()
+                graph[neighbor].remove(leaf)
+
+                if len(graph[neighbor]) == 1:
+                    leaves.append(neighbor)
+        
+        return list(leaves)
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.findMinHeightTrees(4, [[1,0],[1,2],[1,3]]))
+    print(s.findMinHeightTrees(6, [[3,0],[3,1],[3,2],[3,4],[5,4]]))
