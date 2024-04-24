3968a318ec578e2ca4ba4da15f187bf6fba574dc
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-04-24 11:32:07 +0500

    comments

--- Python/113.Path_Sum_II.py
+++ Python/113.Path_Sum_II.py
@@ -6,21 +6,32 @@ class TreeNode:
 
 class Solution:
     def pathSum(self, root: TreeNode, targetSum: int) -> list[list[int]]:
+        # base case: if the root is None, return an empty list
         if not root:
             return root
-        
+        # initialize a list to store all paths
         all_paths = []
-
+        
         def dfs(node, path):
+            # if the current node has a left child, explore it
             if node.left:
+                # recursively call dfs with the left child and update the current path
                 dfs(node.left, path + [node.left.val])
+             # if the current node has a right child, explore it
             if node.right:
+                # recursively call dfs with the right child and update the current path
                 dfs(node.right, path + [node.right.val])
+            # if the current node is a leaf node (no left or right child)
             else:
+                # check if the sum of the current path equals the targetSum    
                 if sum(path) == targetSum and not node.left and not node.right:
+                    # if yes, add the current path to the list of all_paths
                     all_paths.append(path)
         
+        # start the depth-first search from the root node with the initial path 
+        # containing only the root value
         dfs(root, [root.val])
+        # return all the paths that satisfy the condition
         return all_paths
 
 if __name__ == '__main__':
--- Python/114.Flatten_Binary_Tree_to_Linked_List.py
+++ Python/114.Flatten_Binary_Tree_to_Linked_List.py
@@ -6,24 +6,31 @@ class TreeNode(object):
 
 class Solution:
     def flatten(self, root: TreeNode) -> None:
-        """
-        Do not return anything, modify root in-place instead.
-        """
         if not root:
             return
         
         def dfs(node):
+            # store left and right chidren of current node
             node_left, node_right = node.left, node.right
+            # clear left child
             node.left = None
-
+            
+            # if there is a left child, make it right child of current node
             if node_left:
                 node.right = node_left
+                # recursively flatten the left subtree and update the current 
+                # node to the last node of the flattened left subtree
                 node = dfs(node_left)
+            # if there is a right child, make it right child of current node
             if node_right:
                 node.right = node_right
+                # recursively flatten the right subtree and update the current
+                # node to the last node of the flattened right subtree
                 node = dfs(node_right)
-            
+            # return the root of the flattened subtree
             return root
         
+        # start the depth-first search from the root node
         dfs(root)
+        # return the root of the flattened binary tree
         return root
