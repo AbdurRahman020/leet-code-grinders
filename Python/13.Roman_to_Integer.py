dcd150b9be19585dd9f36bea15ad8609ee7c6eb8
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-04-25 08:35:29 +0500

    comments

--- Python/13.Roman_to_Integer.py
+++ Python/13.Roman_to_Integer.py
@@ -1,14 +1,20 @@
 class Solution(object):
     def romanToInt(self, s: str) -> int:
+        # dictionary to map each roman numeral to its corresponding integer value
         roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+        # variable to store the total integer value of the Roman numeral
         total = 0
         
+        # iterate over each character in the input string
         for i in range(len(s)-1):
+            # if the current numeral is smaller than the next numeral, subtract its value
             if roman[s[i]] < roman[s[i+1]]:
                 total -= roman[s[i]]
+            # otherwise, add its value
             else:
                 total += roman[s[i]]
-                
+        
+        # add the value of the last numeral since it wasn't processed in the loop
         return total + roman[s[-1]]
 
 if __name__ == '__main__':
--- Python/146.LRU_Cache.py
+++ Python/146.LRU_Cache.py
@@ -2,21 +2,39 @@ from collections import OrderedDict
 
 class LRUCache:
     def __init__(self, capacity: int):
+        # initialize the LRUCache object with a specific capacity
+        # parameter: capacity (int), the maximum number of key-value pairs the cache can hold
         self.capacity = capacity
+        # using OrderedDict to maintain the order of insertion
         self.cache = OrderedDict()
 
     def get(self, key: int) -> int:
+        # retrieve the value associated with the given key from the cache
+        # parameter: key (int), the key whose value needs to be retrieved
+        # return: the value associated with the key, or -1 if the key is not present 
+        # in the cache
         if key not in self.cache:
             return -1
+        # move the accessed key to the end to indicate it was recently used
         self.cache.move_to_end(key)
         return self.cache[key]
 
     def put(self, key: int, value: int) -> None:
+        # insert or update the value associated with the given key in the cache.
+        # if the key is already present, update its value and move it to the end to indicate
+        # it was recently used
+        # if the key is not present and the cache is full, remove the least recently used 
+        # key-value pair before inserting the new one.
+        # parameters: 1. key (int), The key to be inserted or updated 
+        #             2. value (int), The value associated with the key
         if key in self.cache:
+            # if the key already exists, move it to the end
             self.cache.move_to_end(key)
+        # update the value or insert the new key-value pair
         self.cache[key] = value
         
         if len(self.cache) > self.capacity:
+            # if the cache exceeds its capacity, remove the least recently used key-value pair
             self.cache.popitem(last = False)
 
 if __name__ == '__main__':
