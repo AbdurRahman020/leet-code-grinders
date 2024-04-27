2ea5654739483a17043f31ea0f0bf31fce377f41
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-04-27 08:30:27 +0500

    update

---
+++ Python/206.Reverse_Linked_List.py
@@ -0,0 +1,32 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        # initialize two pointers, prev_node and curr_node
+        # prev_node will initially be None since it's the tail of the reversed list
+        # curr_node starts at the head of the original list
+        prev_node, curr_node = None, head
+        
+        # traverse the original list until curr_node becomes None (end of the list)
+        while curr_node:
+            # store the next node of the current node before modifying it
+            next_node = curr_node.next
+            # reverse the pointer of curr_node to point to the previous node
+            # this effectively reverses the direction of the linked list
+            curr_node.next = prev_node
+            # move prev_node and curr_node one step forward for the next iteration
+            prev_node = curr_node
+            curr_node = next_node
+        
+        # when the loop ends, prev_node will be pointing to the new head of the reversed list
+        # so, return prev_node as the head of the reversed list
+        return prev_node
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.reverseList(ListNode(1, ListNode(2,ListNode(3, ListNode(4, ListNode(5)))))))
+    print(s.reverseList(ListNode(1, ListNode(2))))
+    print((s.reverseList(ListNode())))
