3a529a5b982d25d36ed0139d0372a53f6fb69940
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-05-09 11:31:25 +0500

    comments

--- Python/0387.First_Unique_Character_in_a_String.py
+++ Python/0387.First_Unique_Character_in_a_String.py
@@ -1,5 +1,10 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
+        # list comprehension to iterate over unique characters and find their indices
+        # only consider characters with count 1 (unique)
+        # if there are no unique characters, return -1
+        # use the `min` function to find the minimum index among unique characters, 
+        # or -1 if the list is empty
         return min([s.index(ch) for ch in set(s) if s.count(ch) == 1] or [-1])
 
 if __name__ == '__main__':
