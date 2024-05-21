71e020dc063224c0659ac1a7dd478d0de446f2dc
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-05-21 09:53:31 +0500

    reformat + annotation

--- Python/0079.Word_Search.py
+++ Python/0079.Word_Search.py
@@ -1,5 +1,7 @@
+from typing import List
+
 class Solution(object):
-    def exist(self, board: list[list[str]], word: str) -> bool:
+    def exist(self, board: List[List[str]], word: str) -> bool:
         # get the dimensions of the board
         row_length, col_length = len(board), len(board[0])
         
@@ -9,8 +11,14 @@ class Solution(object):
                 return True
             # if the current position is out of bounds or doesn't match the current character,
             # or if the cell has been visited already, return False
-            if r < 0 or c < 0 or r >= row_length or c >= col_length or \
-              board[r][c] == '' or word[current] != board[r][c]:
+            if (
+                r < 0
+                or c < 0
+                or r >= row_length
+                or c >= col_length
+                or board[r][c] == ''
+                or word[current] != board[r][c]
+            ):
                 return False
             
             # cache the current cell value, mark it as visited
@@ -18,8 +26,12 @@ class Solution(object):
             board[r][c] = ''
             
             # recursively check neighboring cells in all four directions
-            if dfs(r+1, c, current+1) or dfs(r-1, c, current+1) or \
-                 dfs(r, c+1, current+1) or dfs(r, c-1, current+1):
+            if (
+                dfs(r+1, c, current + 1)
+                or dfs(r-1, c, current + 1)
+                or dfs(r, c+1, current + 1)
+                or dfs(r, c-1, current + 1)
+            ):
                 return True
             
             # if the word is not found, backtrack by restoring the original value of the cell
