783f9137796d3dc9e0704539525b8f34189f8a54
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-05-21 08:54:35 +0500

    update

---
+++ Python/0078.Subsets_(Method#1).py
@@ -0,0 +1,30 @@
+from typing import List
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        # get the length of the input list
+        n = len(nums)
+        
+        # define a backtracking function to generate subsets
+        def backtrack(i, path):
+            # append the current subset to the result
+            result.append(path)
+            # iterate over remaining elements to generate more subsets
+            for j in range(i, n):
+                # recursively call backtrack to explore subsets with the current
+                # element included
+                backtrack(j + 1, path + [nums[j]])
+        
+        # initialize an empty list to store subsets
+        result = []
+        # start backtracking from index 0 with an empty subset
+        backtrack(0, [])
+        
+        # return the result list containing all subsets
+        return result
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.subsets([1,2,3]))
+    print(s.subsets([0]))
+    print(s.subsets([9,8,7,6,5]))
---
+++ Python/0078.Subsets_(Method#2).py
@@ -0,0 +1,32 @@
+from typing import List
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        # get the length of the input list
+        n = len(nums)
+        # initialize an empty list to store subsets
+        result = []
+        # iterate over all possible subsets using bit manipulation
+        # 1<<n calculates 2^n, the number of subsets possible
+        for i in range(1<<n):
+            # initialize an empty subset
+            subset = []
+            # iterate over each bit of the current number 'i',
+            # eeach bit represents whether the corresponding element should 
+            # be included in the subset
+            for j in range(n):
+                # check if the j-th bit of i is set
+                if i & (1<<j):
+                    # if set, include the corresponding element from nums in the subset
+                    subset.append(nums[j])
+            # add the generated subset to the result list
+            result.append(subset)
+        
+        # return the result list containing all subsets
+        return result
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.subsets([1,2,3]))
+    print(s.subsets([0]))
+    print(s.subsets([9,8,7,6,5]))
