bd5ec06f5e9d28985e1754255752d7dcb1c2303c
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-05-22 13:22:31 +0500

    update
    previous code was giving error on 3rd test case

--- Python/0437.Path_Sum_III_(BFS).py
+++ Python/0437.Path_Sum_III_(BFS).py
@@ -1,5 +1,5 @@
 from typing import Optional
-from collections import deque, defaultdict
+from collections import deque
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -10,37 +10,26 @@ class TreeNode:
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
         count = 0
-        # if the root is None (empty), return 0 (no path possible)
         if not root:
             return count
         
-        # initialize a queue for BFS traversal
         queue = deque()
-        # start bfs with the root node and its path till that node
         queue.append((root, [root.val]))
         
-        # initialize a defaultdict to store the count of path sums encountered so far
-        path_sums = defaultdict(int)
-        # initialize with 0 sum for paths starting from the root
-        path_sums[0] = 1
-        
-        # perform bfs traversal
         while queue:
-            # extract the current node and the path till that node
-            node, path_till_node = queue.popleft()
-            # calculate the current path sum
-            curr_path_sum = sum(path_till_node)
-            # increment count if there exist any paths with sum equal 
-            # to (current sum - targetSum)
-            count += path_sums[curr_path_sum - targetSum]
-            # update path_sums dictionary with the current path sum
-            path_sums[curr_path_sum] += 1
-            
-            # add child nodes to the queue along with their paths
-            if node.left:
-                queue.append((node.left, path_till_node + [node.left.val]))
-            if node.right:
-                queue.append((node.right, path_till_node + [node.right.val]))
+            n = len(queue)
+            for _ in range(n):
+                node, path_till_node = queue.popleft()
+                curr_path_sum = 0
+                for i in reversed(path_till_node):
+                    curr_path_sum += i
+                    if curr_path_sum == targetSum:
+                        count += 1
+                
+                if node.left:
+                    queue.append((node.left, path_till_node + [node.left.val]))
+                if node.right:
+                    queue.append((node.right, path_till_node + [node.right.val]))
         
         return count
 
@@ -50,3 +39,4 @@ if __name__ == '__main__':
     print(s.pathSum(r1, 8))
     r2 = TreeNode(5, TreeNode(4, TreeNode(11, TreeNode(7), TreeNode(2))), TreeNode(8, TreeNode(13), TreeNode(4, TreeNode(5), TreeNode(1))))
     print(s.pathSum(r2, 22))
+    print(s.pathSum(TreeNode(1, TreeNode(-2), TreeNode(-3)), -1))
