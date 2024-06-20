8f07dd7ea1a6cdbea77fc4588efe934d98afe1e9
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-06-20 09:11:59 +0500

    comments
    added comments

--- Python/0338.Counting_Bits.py
+++ Python/0338.Counting_Bits.py
@@ -1,10 +1,24 @@
 class Solution:
     def countBits(self, n: int) -> list[int]:
+        # initialize an empty list to store the count of bits for each number from 0 to n
         counter = [0]
         
+        # loop through each number from 1 to n
         for i in range(1, n+1):
+            # to count the number of 1s in the binary representation of `i`, we use:
+            # counter[i >> 1] + i % 2
+            #
+            # explanation: i >> 1 shifts the bits of i to the right by 1 position, which effectively
+            # divides i by 2 and floors the result. This gives us the count of 1s in
+            # the binary representation of i // 2
+            #
+            # i % 2 gives the least significant bit of i, which is either 0 or 1
+            #
+            # therefore, counter[i >> 1] + i % 2 gives us the count of 1s in the binary
+            # representation of i and we append this count to the `counter` list
             counter.append(counter[i>>1] + i%2)
         
+        # return the list containing the count of bits for each number from 0 to n
         return counter
 
 if __name__ == '__main__':
--- Python/0345.Reverse_Vowels_of_a_String.py
+++ Python/0345.Reverse_Vowels_of_a_String.py
@@ -1,19 +1,28 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
+        # convert the input string to a list of characters
         s = list(s)
+        # set of vowels
         vowels = set('aeiouAEIOU')
+        # pointers to traverse the string from both ends
         i, j = 0, len(s)-1
         
+        # loop until the two pointers meet
         while i < j:
+            # move pointer `i` until a vowel is found or `i` surpasses `j`
             if s[i] not in vowels:
                 i += 1
+            # move pointer `j` until a vowel is found or `j` surpasses `i`
             elif s[j] not in vowels:
                 j -= 1
             else:
+                # if both `s[i]` and `s[j]` are vowels, swap them
                 s[i], s[j] = s[j], s[i]
+                # move `i` and `j` inward
                 i += 1
                 j -= 1
-                
+        
+        # convert the list of characters back to a string and return
         return ''.join(s)
 
 if __name__ == '__main__':
