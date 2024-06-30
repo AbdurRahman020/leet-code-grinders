af837195ebcd07d48c1945cade2dda1f28963020
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-06-30 10:41:07 +0500

    comment
    removed an unnecessary comment

--- Python/2192.All_Ancestors_of_a_Node_in_a_Directed_Acyclic_Graph_(DFS).py
+++ Python/2192.All_Ancestors_of_a_Node_in_a_Directed_Acyclic_Graph_(DFS).py
@@ -55,6 +55,5 @@ class Solution:
 
 if __name__ == '__main__':
     s = Solution()
-    # Test cases
     print(s.getAncestors(8, [[0,3],[0,4],[1,3],[2,4],[2,7],[3,5],[3,6],[3,7],[4,6]]))
     print(s.getAncestors(5, [[0,1],[0,2],[0,3],[0,4],[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]))
