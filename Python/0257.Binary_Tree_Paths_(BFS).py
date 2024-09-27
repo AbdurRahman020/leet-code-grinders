a182552a9c323d3abd88793650f1bc8edbb60497
A.Rahman Mirza <a.rahmanmirza20@gmail.com>
2024-09-27 14:20:47 +0500

    refactored
    1. modified 'dfs' and also solved with 'bfs'
    2. added the comments
    3. shorten the test cases

---
+++ Python/0257.Binary_Tree_Paths_(BFS).py
@@ -0,0 +1,47 @@
+from typing import Optional, List
+from collections import deque 
+
+class TreeNode():
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution():
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        # if the root is None (empty tree), return an empty list
+        if not root:
+            return []
+        
+        # initialize an empty list to store the paths from root to leaf nodes
+        paths_list = []
+        # initialize a deque with the root node and its value as the starting path
+        queue = deque([(root, str(root.val))])
+        
+        # process nodes in the queue until it's empty
+        while queue:
+            # dequeue the front node and its path
+            curr_node, curr_path = queue.popleft()
+            
+            # check if the current node is a leaf
+            if not curr_node.left and not curr_node.right:
+                # if it's a leaf, add the current path to the paths_list
+                paths_list += [curr_path]
+            
+            # if there is a right child, add it to the queue with the updated path
+            if curr_node.right:
+                queue.append((curr_node.right, curr_path + '->' + str(curr_node.right.val)))
+            
+            # if there is a left child, add it to the queue with the updated path
+            if curr_node.left:
+                queue.append((curr_node.left, curr_path + '->' + str(curr_node.left.val)))
+        
+        # return the list of paths from root to leaf nodes
+        return paths_list                
+                
+        
+if __name__ == '__main__':
+    s = Solution()
+    print(s.binaryTreePaths(TreeNode(1, TreeNode(2, None, TreeNode(5)), TreeNode(3))))
+    print(s.binaryTreePaths(TreeNode(1)))
+    print(s.binaryTreePaths(None))
