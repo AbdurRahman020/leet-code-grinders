3c5cf71eb1a03593eeebd84cc4023934e8b227bb
Abdur Rahman Mirza <166206088+AbdurRahman020@users.noreply.github.com>
2025-01-25 09:02:01 +0500

    refactored

--- Python/0013.Roman_to_Integer.py
+++ Python/0013.Roman_to_Integer.py
@@ -1,21 +1,19 @@
-class Solution(object):
+class Solution:
     def romanToInt(self, s: str) -> int:
-        # dictionary to map each roman numeral to its corresponding integer value
-        roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-        # variable to store the total integer value of the Roman numeral
-        total = 0
+        roman_to_int_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50,'C': 100,
+                            'D': 500, 'M': 1000}
         
-        # iterate over each character in the input string
-        for i in range(len(s)-1):
-            # if the current numeral is smaller than the next numeral, subtract its value
-            if roman[s[i]] < roman[s[i+1]]:
-                total -= roman[s[i]]
-            # otherwise, add its value
+        total, prev = 0, 0
+
+        for char in reversed(s):
+            curr = roman_to_int_map[char]
+            if curr < prev:
+                total -= curr
             else:
-                total += roman[s[i]]
-        
-        # add the value of the last numeral since it wasn't processed in the loop
-        return total + roman[s[-1]]
+                total += curr
+            prev = curr
+
+        return total
 
 if __name__ == '__main__':
     s = Solution()
