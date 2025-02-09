8fa0608ec8710127713a67e7ce40535d6fcc0968
Abdur Rahman Mirza <166206088+AbdurRahman020@users.noreply.github.com>
2025-02-09 22:16:50 +0500

    refactored

--- Python/0026.Remove_Duplicates_from_Sorted_Array.py
+++ Python/0026.Remove_Duplicates_from_Sorted_Array.py
@@ -1,10 +1,21 @@
 from typing import List
 
 class Solution:
-    def removeDuplicates(self, nums: List[int])-> int:
-        nums[:] = sorted(set(nums))
-        return len(nums)
-    
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        i, j = 0, 1
+        
+        while i <= j and j < len(nums):
+            if nums[i] != nums[j]:
+                nums[i + 1] = nums[j]
+                i += 1
+            
+            j += 1
+        
+        return i + 1
+
 if __name__ == '__main__':
     s = Solution()
     print(s.removeDuplicates([1,1,2]))
