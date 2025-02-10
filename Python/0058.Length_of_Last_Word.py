5df253081cb57bc92fb24e25d63a0d76068a143b
Abdur Rahman Mirza <166206088+AbdurRahman020@users.noreply.github.com>
2025-02-10 12:21:40 +0500

    refactored

--- Python/0058.Length_of_Last_Word.py
+++ Python/0058.Length_of_Last_Word.py
@@ -1,12 +1,26 @@
 class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        new_str = s.strip()
-        x = new_str.split(" ")
+    def lengthOfLastWord1(self, s: str) -> int:
+        s = s.strip()
+        count = 0
+
+        for ch in s[::-1]:
+            if ch == " ":
+                break
+
+            count += 1
         
-        return len(x[-1]) 
+        return count
+    
+    def lengthOfLastWord2(self, s: str) -> int:
+        return len(s.strip().split(" ")[-1]) 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.lengthOfLastWord("   fly me   to   the moon  "))
-    print(s.lengthOfLastWord("Hello World"))
-    print(s.lengthOfLastWord("luffy is still joyboy"))
+    
+    print(s.lengthOfLastWord1("   fly me   to   the moon  "))
+    print(s.lengthOfLastWord1("Hello World"))
+    print(s.lengthOfLastWord1("luffy is still joyboy"))
+    
+    print(s.lengthOfLastWord2("   fly me   to   the moon  "))
+    print(s.lengthOfLastWord2("Hello World"))
+    print(s.lengthOfLastWord2("luffy is still joyboy"))
